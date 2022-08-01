19b055fb885da40b020985fb966f7eadbdde3b68
lavesh kumbhalkar <lavesh.kumbhalkar@gmail.com>
2022-08-02 02:14:12 +0530

    new logic

--- Week-6/Ass-1_Max_Sum_Contiguous_Subarray.js
+++ Week-6/Ass-1_Max_Sum_Contiguous_Subarray.js
@@ -1,41 +1,30 @@
 
-const maxSubArraySum = (a, n) =>
-{
-	let maxint = Math.pow(2, 53)
-    // console.log(-maxint-1);
-	let max_so_far = -maxint - 1
-	let max_ending_here = 0
-	
-	for (let i = 0; i < n; i++)
-	{
-		max_ending_here = max_ending_here + a[i]
-        // console.log( max_ending_here);w
 
-		if (max_so_far < max_ending_here)
-        {
-			max_so_far = max_ending_here
-        }
+//Maximum Subarray
 
-		if (max_ending_here < 0)
-        {
-			max_ending_here = 0
-        }
+
+const maxSubArray = (list) => {
+
+	const len = list.length;
+	let max = list[0];
+	let min = 0;
+	let sum = 0;
+	for (let i = 0; i < len; i++) {
+	  sum += list[i];
+	  console.log(sum);
+	  if (sum - min > max) max = sum - min;
+	  if (sum < min) {
+		min = sum;
+	  }
 	}
-	return max_so_far
-}
-
-let a = [ 2, 1, -3, 4, -1, 2, 1, -5, 4];
-
-
-console.log(maxSubArraySum(a, a.length));
-
-
-
-// Time Complexity: O(n)
-// Space Complexity: O(1)
-
-//https://leetcode.com/submissions/detail/760010558/
-
-// TESTING
-
-
+  
+	return max;
+  
+  }
+  
+  const array = [-2,1,-3,4,-1,2,1,-5,4];
+  
+  const lSubArray = maxSubArray(array);
+  
+  
+  console.log(lSubArray); //6
